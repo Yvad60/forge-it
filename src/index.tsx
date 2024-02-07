2e99c901289d1466e857a71627b3bb1861cda705
Ivad <yvesyvad@gmail.com>
2024-02-07 15:26:56 +0200

    test jira:dashboardGadget module

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
-import ForgeUI, { AdminPage, render, Text } from "@forge/ui";
+import ForgeUI, { DashboardGadget, Text, render, useProductContext } from "@forge/ui";
 ForgeUI;
 
-// When using the jira:adminPage the trigger function must return a function that will display the AdminPage
 const App = () => {
+  const {
+    extensionContext: { gadgetConfiguration },
+  } = useProductContext();
+
   return (
-    <AdminPage>
-      <Text>Hello, world! from the admin page app</Text>
-    </AdminPage>
+    <DashboardGadget>
+      <Text>{`Hello ${gadgetConfiguration.name || "world"}`}</Text>
+    </DashboardGadget>
   );
 };
 
